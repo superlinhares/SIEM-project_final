dc66220683615e68f1359a64ebf14325b6ef86dc
superlinhares <pedrolinharesoliveira@gmail.com>
2018-12-16 03:10:55 +0000

    store changes

--- templates/products/list.tpl
+++ templates/products/list.tpl
@@ -9,21 +9,21 @@
   <section id='products'>    
     {foreach from=$products item=$product}
         <article class="product-data">
-            <!-- TODO: Acrescentar as descrições dos produtos-->
-            <!--Clicar no produto passa para a pg do produto -->
-            <!--<h2 class="selected-dish-labels">Nome do prato</h2>-->
-            <h2 class="dish-name">{$product.name}</h2> 
-            <a class="dish-img" href="{$BASE_URL}/pages/store/list_product.php?productID={$product.id}"><img src="{$BASE_URL}/{$product.img}" alt="{$product.id}"></a>
-            <!--TODO: insesir botão de adicionar ao carrinho-->
-            <div class="dish-order">
-              <h2 class="dish-label">Preço / unidade</h2>
-              <p class="dish-label">{$product.price} €</p>
-              <label for="quantity">Quantidade</label>
-              <input type="text" name="quantity" class="product-quantity" value="1">
-              <input type="hidden" name="name" value="{$product.name}">
-              <input type="hidden" name="price" value="{$product.price}">
-              <input type="submit" name="add_to_cart" value="Adicionar ao Carrinho">
-            </div>
+          <!-- TODO: Acrescentar as descrições dos produtos-->
+          <!--Clicar no produto passa para a pg do produto -->
+          <span class="dish-label name-label">Nome do prato</span>
+          <p class="dish-name">{$product.name}</p> 
+          <a class="dish-img" href="{$BASE_URL}/pages/store/list_product.php?productID={$product.id}"><img src="{$BASE_URL}/{$product.img}" alt="{$product.id}"></a>
+          <!--TODO: insesir botão de adicionar ao carrinho-->
+          <div class="dish-order">
+            <span class="dish-label price-label">Preço / unidade</span>
+            <span class="dish-price">{$product.price} €</span>
+            <label class="dish-label quantity-label" for="quantity">Quantidade</label>
+            <input type="text" name="quantity" class="order-quantity" value="1">
+            <input type="hidden" name="name" value="{$product.name}">
+            <input type="hidden" name="price" value="{$product.price}">
+            <input type="submit" class="dish-add-to-cart" name="add_to_cart" value="Adicionar ao Carrinho">
+          </div>
         </article>
     {/foreach}    
   </section>
--- templates_c/89d7828fb477be644616665c4804fa6af477ff58_0.file.list.tpl.php
+++ templates_c/89d7828fb477be644616665c4804fa6af477ff58_0.file.list.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.33, created on 2018-12-16 00:26:11
+/* Smarty version 3.1.33, created on 2018-12-16 06:10:22
   from 'C:\inetpub\wwwroot\project_final\templates\products\list.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.33',
-  'unifunc' => 'content_5c1571733dc9c3_64137480',
+  'unifunc' => 'content_5c15c21e1b8e95_37297008',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '89d7828fb477be644616665c4804fa6af477ff58' => 
     array (
       0 => 'C:\\inetpub\\wwwroot\\project_final\\templates\\products\\list.tpl',
-      1 => 1544909125,
+      1 => 1544929782,
       2 => 'file',
     ),
   ),
@@ -22,7 +22,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
     'file:common/footer.tpl' => 1,
   ),
 ),false)) {
-function content_5c1571733dc9c3_64137480 (Smarty_Internal_Template $_smarty_tpl) {
+function content_5c15c21e1b8e95_37297008 (Smarty_Internal_Template $_smarty_tpl) {
 ?><!-- Header -->
 <?php $_smarty_tpl->_subTemplateRender("file:common/header.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
 ?>
@@ -39,30 +39,30 @@ if ($_from !== null) {
 foreach ($_from as $_smarty_tpl->tpl_vars['product']->value) {
 ?>
         <article class="product-data">
-            <!-- TODO: Acrescentar as descrições dos produtos-->
-            <!--Clicar no produto passa para a pg do produto -->
-            <!--<h2 class="selected-dish-labels">Nome do prato</h2>-->
-            <h2 class="dish-name"><?php echo $_smarty_tpl->tpl_vars['product']->value['name'];?>
-</h2> 
-            <a class="dish-img" href="<?php echo $_smarty_tpl->tpl_vars['BASE_URL']->value;?>
+          <!-- TODO: Acrescentar as descrições dos produtos-->
+          <!--Clicar no produto passa para a pg do produto -->
+          <span class="dish-label name-label">Nome do prato</span>
+          <p class="dish-name"><?php echo $_smarty_tpl->tpl_vars['product']->value['name'];?>
+</p> 
+          <a class="dish-img" href="<?php echo $_smarty_tpl->tpl_vars['BASE_URL']->value;?>
 /pages/store/list_product.php?productID=<?php echo $_smarty_tpl->tpl_vars['product']->value['id'];?>
 "><img src="<?php echo $_smarty_tpl->tpl_vars['BASE_URL']->value;?>
 /<?php echo $_smarty_tpl->tpl_vars['product']->value['img'];?>
 " alt="<?php echo $_smarty_tpl->tpl_vars['product']->value['id'];?>
 "></a>
-            <!--TODO: insesir botão de adicionar ao carrinho-->
-            <div class="dish-order">
-              <h2 class="dish-label">Preço / unidade</h2>
-              <p class="dish-label"><?php echo $_smarty_tpl->tpl_vars['product']->value['price'];?>
- €</p>
-              <label for="quantity">Quantidade</label>
-              <input type="text" name="quantity" class="product-quantity" value="1">
-              <input type="hidden" name="name" value="<?php echo $_smarty_tpl->tpl_vars['product']->value['name'];?>
+          <!--TODO: insesir botão de adicionar ao carrinho-->
+          <div class="dish-order">
+            <span class="dish-label price-label">Preço / unidade</span>
+            <span class="dish-price"><?php echo $_smarty_tpl->tpl_vars['product']->value['price'];?>
+ €</span>
+            <label class="dish-label quantity-label" for="quantity">Quantidade</label>
+            <input type="text" name="quantity" class="order-quantity" value="1">
+            <input type="hidden" name="name" value="<?php echo $_smarty_tpl->tpl_vars['product']->value['name'];?>
 ">
-              <input type="hidden" name="price" value="<?php echo $_smarty_tpl->tpl_vars['product']->value['price'];?>
+            <input type="hidden" name="price" value="<?php echo $_smarty_tpl->tpl_vars['product']->value['price'];?>
 ">
-              <input type="submit" name="add_to_cart" value="Adicionar ao Carrinho">
-            </div>
+            <input type="submit" class="dish-add-to-cart" name="add_to_cart" value="Adicionar ao Carrinho">
+          </div>
         </article>
     <?php
 }
